ed77dc0331c3a51d5cbb0cfd7a56e5b359331f0b
User <yarik.sawchuck3102004@gmail.com>
2024-11-20 18:58:40 +0200

    Manage Users - Create, Read, Delete

---
+++ Bloggie.Web/Controllers/AdminUsersController.cs
@@ -0,0 +1,94 @@
+﻿using Bloggie.Web.Models.ViewModels;
+using Bloggie.Web.Repositories;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bloggie.Web.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class AdminUsersController : Controller
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public AdminUsersController(IUserRepository userRepository,
+            UserManager<IdentityUser> userManager)
+        {
+            _userRepository = userRepository;
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> List()
+        {
+            var users = await _userRepository.GetAll();
+
+            var usersViewModel = new UserViewModel();
+            usersViewModel.Users = new List<User>();
+
+            foreach (var user in users)
+            {
+                usersViewModel.Users.Add(new User
+                {
+                    Id = Guid.Parse(user.Id),
+                    Username = user.UserName,
+                    Email = user.Email
+                });
+            }
+
+            return View(usersViewModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> List(UserViewModel request)
+        {
+            var identityUser = new IdentityUser
+            {
+                UserName = request.Username,
+                Email = request.Email
+            };
+
+            var identityResult = await _userManager.CreateAsync(identityUser, request.Password);
+
+            if (identityResult is not null)
+            {
+                if (identityResult.Succeeded)
+                {
+                    //assign roles to this user
+                    var roles = new List<string> { "User" };
+                    if (request.AdminRoleCheckBox)
+                    {
+                        roles.Add("Admin");
+                    }
+
+                    identityResult = await _userManager.AddToRolesAsync(identityUser, roles);
+
+                    if (identityResult is not null && identityResult.Succeeded)
+                    {
+                        return RedirectToAction("List", "AdminUsers");
+                    }
+                }
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var user =await _userManager.FindByIdAsync(id.ToString());
+
+            if (user is not null)
+            {
+                var identityResult = await _userManager.DeleteAsync(user);
+                if (identityResult is not null && identityResult.Succeeded)
+                {
+                    return RedirectToAction("List", "AdminUsers");
+                }
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ Bloggie.Web/Repositories/UserRepository.cs
@@ -0,0 +1,30 @@
+﻿using Bloggie.Web.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bloggie.Web.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly AuthDbContext _context;
+
+        public UserRepository(AuthDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<IdentityUser>> GetAll()
+        {
+            var users = await _context.Users.ToListAsync();
+
+            var superAdminUser = await _context.Users.FirstOrDefaultAsync(x => x.Email == "superadmin@bloggie.com");
+
+            if (superAdminUser != null)
+            {
+                users.Remove(superAdminUser);
+            }
+
+            return users;
+        }
+    }
+}
